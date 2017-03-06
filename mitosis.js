c60a2b5a4290f08ca850877dafaed8272b507858
John Sanders <jws121295@gmail.com>
2017-03-05 20:49:11 -0500

    Symmetry!!

--- mitosis.js
+++ mitosis.js
@@ -39,6 +39,7 @@ Quadrant.prototype = Object.create(Two.Group.prototype);
 
 
 var Surface = function Surface() {
+  // Default coords and super constructor
   var points = arguments;
   if (points.length === 0) {
     points = [
@@ -50,24 +51,46 @@ var Surface = function Surface() {
   }
   Two.Path.call(this, points);
 
-  q1.add(this);
+  this.symmentries = []; // an index of all quadrants that should contain mirrors of this path
+  this.velocity = new Two.Vector(0, 0);
+
+  this.addTo = function (quadrant) {
+    var symPath = this.symmentries.length === 0 ? this : this.clone();
+    symPath.translation = this.translation;
+    this.symmentries.push(symPath);
+    quadrant.add(symPath);
+  }
+
+  var oldUpdate = this._update;
+  this._update = function () {
+    if (this.velocity.x !== 0 || this.velocity.y !== 0)
+      this.translation.addSelf(this.velocity);
+    oldUpdate.apply(this, arguments);
+  }
 };
 Surface.prototype = Object.create(Two.Path.prototype);
 
 
+// Quadrant setup
 var q1 = new Quadrant();
 var q2 = new Quadrant();
 var q3 = new Quadrant();
 var q4 = new Quadrant();
-
 q2.scale = new Two.Vector(-1, 1);
 q3.scale = new Two.Vector(-1, -1);
 q4.scale = new Two.Vector(1, -1);
 
+// Surface setup
 var rect = new Surface();
 rect.scale = 256;
+rect.translation.set(-128, -128);
 rect.fill = "red";
 rect.noStroke();
+rect.addTo(q1);
+rect.addTo(q2);
+rect.addTo(q3);
+rect.addTo(q4);
 
+// kickoff
 two.trigger("resize");
 two.play();
