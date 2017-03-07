bf16b750dd7a19c1436051d555e3c1ad3fedfa19
John Sanders <jws121295@gmail.com>
2017-03-07 16:09:53 -0500

    proper rotation and scaling, rotational movement, regeneration, random offsets,  and stroke or fill

--- mitosis.js
+++ mitosis.js
@@ -1,5 +1,6 @@
 // jshint browser:true
 /* globals Two */
+
 // Bindings
 var two = new Two().appendTo(document.body);
 two.on("resize", function () {
@@ -41,22 +42,36 @@ Quadrant.prototype = Object.create(Two.Group.prototype);
 var Surface = function Surface() {
   // Default coords and super constructor
   var points = arguments;
+
+  var variance = new Two.Vector(Math.random(), Math.random());
+  variance.multiplyScalar(.9).addSelf(new Two.Vector(.1,.1));
+
+  var scale = variance.clone().multiplyScalar(Math.max(two.width*.7, two.height*.7));
+
+  // default shape
   if (points.length === 0) {
     points = [
-      new Two.Anchor(-0.5, -0.5),
-      new Two.Anchor(0.5, -0.5),
-      new Two.Anchor(0.5, 0.5),
-      new Two.Anchor(-0.5, 0.5)
+      new Two.Anchor(-scale.x/2, -scale.y/2),
+      new Two.Anchor(scale.x/2, -scale.y/2),
+      new Two.Anchor(scale.x/2, scale.y/2),
+      new Two.Anchor(-scale.x/2, scale.y/2)
     ];
   }
+
+  // rotate object
+  var rotation = Math.random()*Math.PI/2;
+  points.forEach(function(point){
+    point = point.rotate(rotation);
+  });
+  var rotationSpeed = Math.random()*.01 -.005;
   Two.Path.call(this, points);
 
   this.symmetries = []; // an index of all quadrants that should contain mirrors of this path
   this.velocity = new Two.Vector(0, 0);
-  this.scale = new Two.Vector(1, 1);
 
   this.addTo = function (quadrant) {
     var symPath = this.symmetries.length === 0 ? this : this.clone();
+    symPath.rotation = this.rotation;
     symPath.translation = this.translation;
     this.symmetries.push(symPath);
     quadrant.add(symPath);
@@ -64,13 +79,17 @@ var Surface = function Surface() {
 
   var self = this;
   this.update = function(){
-    self.velocity.multiplyScalar(1.01);
+    self.velocity.multiplyScalar(1.005);
     if (self.velocity.x !== 0 || self.velocity.y !== 0)
       self.translation.addSelf(self.velocity);
 
     var bounds = self.getBoundingClientRect();
     if(bounds.left > two.width || bounds.top > two.height)
       self.remove();
+
+    self.symmetries.forEach(function(symmetry){
+      symmetry.rotation+=rotationSpeed;
+    });
   }
 
   this._update();
@@ -82,31 +101,34 @@ var Surface = function Surface() {
       symmetry.parent.remove(symmetry);
     });
     this.stopListening(two, Two.Events.update, this.update);
+    setTimeout(function(){
+      new Surface();
+    }, 1000*Math.random());
   }
 
   // visual defaults
-  this.scale.set(two.width/2, two.height/2);
-  var variance = new Two.Vector(Math.random(),Math.random()).normalize();
-  variance.multiplyScalar(0.9); // max
-  variance.addSelf(new Two.Vector(0.1, 0.1)); // min
-  this.scale.multiplySelf(variance);
-  this.rotation = Math.PI/2 * Math.random();
   var bounds = this.getBoundingClientRect();
-  this.translation.set(-this.scale.x/2, -this.scale.y/2);
+  this.translation.set(-bounds.width/2, -bounds.height/2);
+  if(Math.random() > .5){
+    this.translation.addSelf(new Two.Vector(Math.random()*two.width/2,0));
+  } else {
+    this.translation.addSelf(new Two.Vector(0,Math.random()*two.height/2));
+  }
 
   this.fill = "rgba(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ",.5)";
+  this.stroke = this.fill;
+  this.linewidth = 1+9*Math.random();
 
-//  var speedPercent = Math.sqrt(bounds.width * bounds.width + bounds.height * bounds.height) / Math.sqrt(two.width * two.width + two.height * two.height);
-//  console.log(speedPercent);
-//  var speed = .5 + 4.5 * speedPercent;
   var inverseVariance = new Two.Vector(1 - variance.x, 1 - variance.y);
-  var speed = inverseVariance;
+  var speed = inverseVariance.multiplyScalar(.2).length();
   var direction = Math.random()*Math.PI/2;
-  this.velocity.set(Math.cos(direction)*speed.x, Math.sin(direction)*speed.y);
-//  this.velocity.addSelf(new Two.Vector(1,1));
+  this.velocity.set(Math.cos(direction)*speed, Math.sin(direction)*speed);
 
-
-  this.noStroke();
+  if(Math.random()>.5){
+    this.noStroke();
+  }else{
+    this.noFill();
+  }
 
   this.addTo(q1);
   this.addTo(q2);
@@ -126,7 +148,7 @@ q3.scale = new Two.Vector(-1, -1);
 q4.scale = new Two.Vector(1, -1);
 
 // Surface setup
-//var rect = new Surface();
+var rect = new Surface();
 
 // kickoff
 two.trigger("resize");
