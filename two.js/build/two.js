618e89d57f81a464e1b1533dc26303476be4f174
John Sanders <jws121295@gmail.com>
2017-03-07 16:08:21 -0500

    rotate function

--- two.js/build/two.js
+++ two.js/build/two.js
@@ -2460,6 +2460,13 @@ this.Two = (function(previousTwo) {
 
     toObject: function() {
       return { x: this.x, y: this.y };
+    },
+
+    rotate: function(radians) {
+      var newX = this.x*Math.cos(radians) - this.y*Math.sin(radians);
+      var newY = this.x*Math.sin(radians) + this.y*Math.cos(radians);
+      this.set(newX,newY);
+      return this;
     }
 
   });
